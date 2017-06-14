00d37c6484c8813f9e4def2d5fb8f6247b44953a
Orlando Acevedo-Charry <acevedocharry@gmail.com>
2017-06-13 20:24:18 -0400

    Create Rcode - Map.R

--- Rcode - Map.R
+++ Rcode - Map.R
@@ -9,13 +9,3 @@ library(maptools)
 data(wrld_simpl)
 plot(wrld_simpl, xlim=c(-100,-50),ylim=c(-10,20), axes=T,col="light gray")
 points(tzgeo$lon,tzgeo$lat,col='white',pch=20,cex=1)
-
-Nystalus=gbif("nystalus","obamai*",geo=F)
-colnames(Nystalus)
-nsgeo<-subset(Nystalus,!is.na(lon) & !is.na(lat))
-nsgeo[1:4,c(1:5,7:10)]
-write.csv(Nystalus,file="Nobamai_gbif.csv")
-library(maptools)
-data(wrld_simpl)
-plot(wrld_simpl, xlim=c(-100,-50),ylim=c(-10,20), axes=T,col="light gray")
-points(nsgeo$lon,nsgeo$lat,col='white',pch=20,cex=1)
